66cd735d1de8d7752435d2613a38eee12da48eec
toneswari <toneswarinandam037@gmail.com>
2023-07-03 14:44:04 +0530

    added content in logincontroller

---
+++ app/Controllers/Login_controller.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+class Login_controller extends Controller{
+    public function index()
+{
+    // return view('welcome_message');
+    echo "hi welcome to this website";
+    // $this->load->view('login');
+    
+}
+}
